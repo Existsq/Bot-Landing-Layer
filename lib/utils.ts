51b5bb666e103052509086525f9766640ff74d63
Exist <existqq@gmail.com>
2024-08-10 12:08:38 +0300

    add tailwind-merge

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,5 +1,5 @@
 import { ClassValue, clsx } from "clsx";
-import { twMerge } from "@/node_modules/tailwind-merge/src/lib/tw-merge";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
